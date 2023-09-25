9f73f8076324a50471b3e8c9cc43cd4e89c838a1
SSerg-dev <srmail@mail.ru>
2023-09-25 17:37:23 +0300

    add interceptor filters functions and Order.vue component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Order from '../views/Order.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/Home.vue')
+    }, 
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('../views/Order.vue')
     }
-    
   ]
 })
 
